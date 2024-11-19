3100085dbebfc9c0865f47dd6f4793228b71a572
Mário Luiz  Guimarães <103617965+marioluizguimaraes@users.noreply.github.com>
2024-11-19 10:15:29 -0300

    .

---
+++ Indentificador de Mesma Rede/src/EnderecoIP.java
@@ -0,0 +1,12 @@
+public class EnderecoIP {
+
+    public EnderecoIP(String mascara, String enderecpIp){
+        int[] mascaraDeRede = new int[4];
+        int[] meuIp = new int[4];
+
+        for (int i = 0; i < 4; i++){
+
+        }
+    }
+
+}

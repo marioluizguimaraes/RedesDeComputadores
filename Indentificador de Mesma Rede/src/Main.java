d548c707e33a60399d9797a468054de4fd151f32
Mário Luiz  Guimarães <mariolsg.oficial@gmail.com>
2024-11-26 18:35:41 -0300

    .

--- Indentificador de Mesma Rede/src/Main.java
+++ Indentificador de Mesma Rede/src/Main.java
@@ -1,16 +1,30 @@
 import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        EnderecoIP ip1 = new EnderecoIP("10.10.10.254", "255.255.254.0");
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro endereço IP: ");
+        String enderecoip1 = entrada.next();
+
+        System.out.print("Digite o segundo endereço IP: ");
+        String enderecoip2 = entrada.next();
+
+        System.out.print("Digite a mascara de rede: ");
+        String mascara = entrada.next();
+
+        System.out.println("-------------------------------------------------------------");
+        EnderecoIP ip1 = new EnderecoIP(enderecoip1, mascara);
         System.out.println("Endereço Ip: " + ip1.getIp());
         System.out.println("Mascara: " + ip1.getMascara());
         System.out.println("Endereço Broadcast: " + ip1.broadcastDaRede(ip1.getMascara()));
 
         System.out.println("-------------------------------------------------------------");
 
-        EnderecoIP ip2 = new EnderecoIP("10.10.12.0", "255.255.254.0");
+        EnderecoIP ip2 = new EnderecoIP(enderecoip2, mascara);
         System.out.println("Endereço Ip: " + ip2.getIp());
         System.out.println("Mascara: " + ip2.getMascara());
         System.out.println("Endereço Broadcast: " + ip2.broadcastDaRede(ip2.getMascara()));

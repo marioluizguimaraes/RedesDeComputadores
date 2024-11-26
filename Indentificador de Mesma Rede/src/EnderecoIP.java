7ca7e6ff83b44780bc0fe53c6e56e821b2188adb
Mário Luiz  Guimarães <mariolsg.oficial@gmail.com>
2024-11-26 07:54:51 -0300

    .

--- Indentificador de Mesma Rede/src/EnderecoIP.java
+++ Indentificador de Mesma Rede/src/EnderecoIP.java
@@ -1,12 +1,44 @@
+import java.util.Arrays;
+
 public class EnderecoIP {
+    private String enderecoIp = "";
+    private String mascaraDeRede = "";
 
-    public EnderecoIP(String mascara, String enderecpIp){
-        int[] mascaraDeRede = new int[4];
-        int[] meuIp = new int[4];
+    private String[] binarioCodeIp = new String[4];
+    private String[] binarioCodeMascara = new String[4];
 
-        for (int i = 0; i < 4; i++){
+    public EnderecoIP (String ip, String mascara){
+        enderecoIp = ip;
+        mascaraDeRede = mascara;
 
+        String[] StringDeOctetosDeIp = this.enderecoIp.split("\\.");
+        String[] StringDeOctetosDeMascara = this.mascaraDeRede.split("\\.");
+
+        int[] octetosDeIp = Arrays.stream(StringDeOctetosDeIp).mapToInt(Integer::parseInt).toArray();
+        int[] octetosDeMascara = Arrays.stream(StringDeOctetosDeMascara).mapToInt(Integer::parseInt).toArray();
+
+        //----------------------------------------------------------------------------------------------------------------
+
+        for (int i = 0; i < 4; i++) {
+            binarioCodeIp[i] = String.format("%08d", Integer.parseInt(Integer.toBinaryString(octetosDeIp[i])));
+            binarioCodeMascara[i] = String.format("%08d", Integer.parseInt(Integer.toBinaryString(octetosDeMascara[i])));
         }
+    }
+
+    public String inverterBits(String binario) {
+        StringBuilder invertido = new StringBuilder();
+        for (char bit : binario.toCharArray()) {
+            invertido.append(bit == '0' ? '1' : '0'); // Inverte 0 para 1 e 1 para 0
+        }
+        return invertido.toString();
+    }
+
+    public String getIp(){
+        return String.join(".", binarioCodeIp);
+    }
+
+    public String getMascara(){
+        return String.join(".", binarioCodeMascara);
     }
 
 }
--- Indentificador de Mesma Rede/src/Main.java
+++ Indentificador de Mesma Rede/src/Main.java
@@ -1,5 +1,10 @@
-public class Main {
-    public static void main(String[] args) {
 
+public class Main {
+
+    public static void main(String[] args) {
+        EnderecoIP ip1 = new EnderecoIP("10.10.10.255", "255.255.255.0");
+        System.out.println(ip1.getIp());
+        System.out.println(ip1.getMascara());
+        System.out.println(ip1.inverterBits(ip1.getMascara()));
     }
 }

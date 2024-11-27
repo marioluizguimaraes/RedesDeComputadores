7a10580a70b9481d4acc64609b2e8093b181e359
Mário Luiz  Guimarães <mariolsg.oficial@gmail.com>
2024-11-27 16:05:32 -0300

    .

--- Indentificador de Mesma Rede/src/Main.java
+++ Indentificador de Mesma Rede/src/Main.java
@@ -7,20 +7,29 @@ public class Main {
 
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("-------------------------------------------------------------");
-
-        System.out.println("ATENÇÃO: O IP e a Mascara deve ser digitado no formato 255.255.255.255");
-
-        System.out.println("-------------------------------------------------------------");
-
         System.out.print("Digite o primeiro endereço IP: ");
         String enderecoip1 = entrada.next();
+
+        if (!validarEnderecoIP(enderecoip1)) {
+            System.out.println("Endereço IP inválido!");
+            return;
+        }
 
         System.out.print("Digite o segundo endereço IP: ");
         String enderecoip2 = entrada.next();
 
+        if (!validarEnderecoIP(enderecoip2)) {
+            System.out.println("Endereço IP inválido!");
+            return;
+        }
+
         System.out.print("Digite a mascara de rede: ");
         String mascara = entrada.next();
+
+        if (!validarMascaraDeRede(mascara)) {
+            System.out.println("Máscara de rede inválida!");
+            return;
+        }
 
         System.out.println("-------------------------------------------------------------");
 
@@ -39,5 +48,40 @@ public class Main {
         System.out.println("-------------------------------------------------------------");
 
         System.out.println(Objects.equals(ip1.broadcastDaRede(ip1.getMascara()), ip2.broadcastDaRede(ip2.getMascara())) ? "SIM" : "NÃO");
+    }
+
+    public static boolean validarEnderecoIP(String endereco) {
+        String[] octetos = endereco.split("\\.");
+
+        if (octetos.length != 4) return false;
+
+        try {
+            for (String octeto : octetos) {
+                if (Integer.parseInt(octeto) < 0 || Integer.parseInt(octeto) > 255) return false;
+            }
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean validarMascaraDeRede(String mascara) {
+        String[] octetos = mascara.split("\\.");
+
+        if (octetos.length != 4) return false;
+
+        try {
+            int[] valores = new int[4];
+            for (int i = 0; i < 4; i++) {
+                valores[i] = Integer.parseInt(octetos[i]);
+                if (valores[i] < 0 || valores[i] > 255) return false;
+            }
+
+            for (int i = 0; i < valores.length; i++) {
+                if (valores[i] < 255 && valores[i-1] < 255) return false;
+            }
+        } catch (NumberFormatException e) { return false; }
+        return true;
     }
 }
